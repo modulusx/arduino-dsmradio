6da23e86754418dc025879a79c1564d9a1210dc5
Matt Kunkel <matt@mrkunkel.com>
2017-10-17 20:37:40 -0700

    Minor update

--- Library/DSMRadio.cpp
+++ Library/DSMRadio.cpp
@@ -1,14 +1,15 @@
-#include "WProgram.h"
+#include <Arduino.h>
 #include "DSMRadio.h"
+
 #define BIND_HEADER 0x80
 #define DSMRADIO_CHANNELS 6
-#define DSMRADIO_FRAME_LEN 14
+#define DSMRADIO_FRAMELEN 14
 
 void DSMRadio::bind(bool rebind) {
   Serial.begin(125000);
   delay(100);
   if (rebind) frame[0] = BIND_HEADER;
-  Serial.write(frame,DSMRADIO_FRAME_LEN);
+  Serial.write(frame,DSMRADIO_FRAMELEN);
   delay(20);
   frame[0]=0;
 }
@@ -19,6 +20,6 @@ void DSMRadio::send(unsigned int channels[]) {
     frame[2*c+2] = (byte)(c<<2) | highByte(channels[c]);
     frame[2*c+3] = lowByte(channels[c]);
   }
-  Serial.write(frame,DSMRADIO_FRAME_LEN);
+  Serial.write(frame,DSMRADIO_FRAMELEN);
   delay(20);
 }
--- Library/DSMRadio.h
+++ Library/DSMRadio.h
@@ -1,16 +1,16 @@
 #ifndef DSMRadio_h
 #define DSMRadio_h
 
-#include "WProgram.h"
+#include <Arduino.h>
 
 #define DSMRADIO_CHANNELS 6
-#define DSMRADIO_FRAME_LEN 14
+#define DSMRADIO_FRAMELEN 14
 
 class DSMRadio {
   public:
     void bind(bool rebind);
     void send(unsigned int channels[DSMRADIO_CHANNELS]);
   private:
-    byte frame[DSMRADIO_FRAME_LEN];
+    byte frame[DSMRADIO_FRAMELEN];
 };
 #endif
